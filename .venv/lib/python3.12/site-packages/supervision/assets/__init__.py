9f6eda40eb1e7884fa6ac944b1922126646f4e3d
hichem2100 <ajinahichem@gmail.com>
2026-08-15 15:02:31 +0100

    Stop tracking files listed in .gitignore

--- .venv/lib/python3.12/site-packages/supervision/assets/__init__.py
+++
@@ -1,4 +0,0 @@
-from supervision.assets.downloader import download_assets
-from supervision.assets.list import ImageAssets, VideoAssets
-
-__all__ = ["ImageAssets", "VideoAssets", "download_assets"]
--- .venv/lib/python3.12/site-packages/supervision/metrics/utils/utils.py
+++
@@ -1,9 +0,0 @@
-def ensure_pandas_installed() -> None:
-    try:
-        import pandas  # noqa
-    except ImportError:
-        raise ImportError(
-            "`metrics` extra is required to run the function."
-            " Run `uv pip install 'supervision[metrics]'` or"
-            " `uv add supervision --extra metrics`."
-        )
